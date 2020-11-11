6ea2ea19b9a5d420b35520a73828f3168a060948
Jake <jakegreen2016@gmail.com>
2020-11-10 15:36:29 -0900

    add pseudo-code

---
+++ master_mind.rb
@@ -0,0 +1,48 @@
+# Module with variables
+
+# Class for computer
+
+  # Select a random the "code-sequence"
+
+  # Check player's guess and provide feedback
+
+    # A correct entry (in placement and color)
+
+    # A correct color-only
+
+    # A cracked code
+
+# Class for player
+    
+    # Guess "code-sequence"
+
+# Class for the running the game
+
+  # Establish no. of games to play, must be an even no.
+
+  #For each game
+
+    # Play each game
+    
+      # 1. Computer makes "code"
+      
+      # 2. Player guesses
+
+      # 3. Computer provides feed-back
+
+      # 4. Repeat steps 2 and 3 until game is over
+
+    # Ending each game
+
+        # When computer declares code is cracked
+    
+        # When there are no more guesses
+
+        # Check if there are more games
+
+          # If there are more games to play, start another one
+
+          # If all games are played, end the session
+
+
+        
